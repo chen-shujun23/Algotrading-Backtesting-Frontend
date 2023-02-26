6860ac5fafbf7edcb39625dba266d8e144df440f
Shujun Chen <c.shujun23@gmail.com>
2023-02-26 17:51:40 +0800

    Set up tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h2 className="bg-primary-400 font-3xl">GA SEI-41</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        15: "repeat(15, minmax(0, 1fr))",
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        peach: "#FDE5D8",
+        primary: {
+          800: "#2B4435",
+          600: "#4F7F71",
+          400: "#99B8A3",
+          200: "#D0DFD5",
+        },
+        secondary: {
+          600: "#C7CBD0",
+          400: "#E3EAF3",
+          200: "#F3F8FF",
+        },
+      },
+
+      fontFamily: {
+        DM: ["DM Sans", "sans-serif"],
+      },
+      fontSize: {
+        "3xl": "26px",
+        xl: "20px",
+        lg: "18px",
+        base: "16px",
+      },
+    },
+  },
+  plugins: [],
+};

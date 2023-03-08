f372ffc91f1b1495e3d45284232e62202e6935a5
Shujun Chen <c.shujun23@gmail.com>
2023-03-09 01:16:35 +0800

    Fetch getAllUsers data

--- src/components/UsersTable.jsx
+++ src/components/UsersTable.jsx
@@ -1,40 +1,56 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Button from "./Button";
+import useAxios from "../hooks/useAxios";
+import config from "../../config.js";
+import { GlobalContext } from "../App";
 
-const TableAdmin = () => {
-  const admin = [
-    {
-      name: "Lindsay Walton",
-      title: "Junior Administrator",
-      email: "lindsay.walton@example.com",
-    },
-    {
-      name: "Charlie Brown",
-      title: "Senior Administrator",
-      email: "lindsay.walton@example.com",
-    },
-    {
-      name: "Archie Andrews",
-      title: "Senior Administrator",
-      email: "archie.andrews@example.com",
-    },
-  ];
+const UsersTable = (props) => {
+  const { accessToken } = useContext(GlobalContext);
+  const [data, error, loading, fetchData] = useAxios();
+  const [adminUsers, setAdminUsers] = useState([]);
+  const [nonAdminUsers, setNonAdminUsers] = useState([]);
+
+  const getAllUsers = () => {
+    const url = config.BASE_URL + "/users/all-users";
+    const method = "GET";
+    const body = null;
+    const token = accessToken;
+    fetchData(url, method, body, token);
+  };
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  useEffect(() => {
+    if (data) {
+      setAdminUsers(data.filter((user) => user.is_admin === true));
+      setNonAdminUsers(data.filter((user) => user.is_admin === false));
+    }
+  }, [data]);
+
+  const user = props.admin ? adminUsers : nonAdminUsers;
+
   return (
-    <div className="bg-yellow-light pt-20">
+    <div className="bg-yellow-light pt-10">
       <div className="flex items-center">
         <div className="flex-auto">
-          <h2>Admin</h2>
+          <h2>{props.admin ? "Admin" : "Users"}</h2>
           <span className="mt-2 text-brown">
-            A list of all the administrators in your organisation.
+            {props.admin
+              ? "A list of all the administrators in your organisation."
+              : "A list of active user accounts."}
           </span>
         </div>
         <div className="mt-4 mt-0 ml-16 flex-none">
-          <Button
-            type="button"
-            bgColour="bg-red"
-            innerText="Add Admin"
-            link="/admin-register"
-          />
+          {props.admin ? (
+            <Button
+              type="button"
+              bgColour="bg-red"
+              innerText="Add Admin"
+              link="/user-register"
+            />
+          ) : null}
         </div>
       </div>
       <div className="mt-8 flow-root">
@@ -47,19 +63,19 @@ const TableAdmin = () => {
                     scope="col"
                     className="p-4 text-left font-semibold text-brown pl-6"
                   >
-                    Name
+                    First Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="p-4 text-left font-semibold text-brown"
+                  >
+                    Last Name
                   </th>
                   <th
                     scope="col"
                     className="p-4 text-left font-semibold text-brown"
                   >
                     Email
-                  </th>
-                  <th
-                    scope="col"
-                    className="p-4 text-left font-semibold text-brown"
-                  >
-                    Title
                   </th>
 
                   <th scope="col" className="p-4 text-left">
@@ -71,17 +87,18 @@ const TableAdmin = () => {
                 </tr>
               </thead>
               <tbody className="divide-y divide-brown bg-yellow-light">
-                {admin.map((person) => (
+                {user.map((person) => (
                   <tr key={person.email}>
                     <td className="whitespace-nowrap p-4 text-sm text-brown font-semibold pl-6">
-                      {person.name}
+                      {person.first_name}
+                    </td>
+                    <td className="whitespace-nowrap p-4 text-sm text-brown">
+                      {person.last_name}
                     </td>
                     <td className="whitespace-nowrap p-4 text-sm text-brown">
                       {person.email}
                     </td>
-                    <td className="whitespace-nowrap p-4 text-sm text-brown">
-                      {person.title}
-                    </td>
+
                     <td className="whitespace-nowrap p-4 text-sm">
                       <a href="#" className="text-blue hover:underline">
                         Delete
@@ -103,4 +120,4 @@ const TableAdmin = () => {
   );
 };
 
-export default TableAdmin;
+export default UsersTable;
--- src/pages/AdminHome.jsx
+++ src/pages/AdminHome.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import PageHeader from "../components/PageHeader";
-import UsersTable from "../components/TableUsers";
-import TableAdmin from "../components/TableAdmin";
+import UsersTable from "../components/UsersTable";
 import illustration from "../assets/illustrationAdminDashboard.png";
 import { GlobalContext } from "../App";
 
@@ -11,13 +10,12 @@ const AdminHome = () => {
     <div>
       <PageHeader
         header={`Welcome back, ${userPayload.first_name}`}
-        copy='"The only way to do great work is to love what you do." - Steve Jobs.'
         imgSrc={illustration}
         imgAlt="Graphic illustration of a woman with a laptop."
       />
       <div className="p-20 bg-yellow-light">
-        <UsersTable />
-        <TableAdmin />
+        <UsersTable admin={false} />
+        <UsersTable admin={true} />
       </div>
     </div>
   );

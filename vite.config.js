e348eae1b9e0188a4ac29f269d17704e1ea42a42
Shujun Chen <c.shujun23@gmail.com>
2023-04-23 16:29:56 +0800

    Change localhost to 3000

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [
     react(),
     nodePolyfills({
